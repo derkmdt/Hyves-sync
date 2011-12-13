d3edff2a7ffc42378793b81f16559005923be277
nicolasbrailo <nicolasbrailo@gmail.com>
2011-12-13 16:13:14 +0100

    Added new config files with different include paths

--- config.php
+++ config.php
@@ -1,4 +1,15 @@
 <?
-//set_include_path('/home/nico/hackathon/www/ext_libs/');
-set_include_path('/Users/derkbraakman/Documents/Hyves-sync/ext_libs/');
+$inc_paths_nico = array(
+                        '/home/nico/hackathon/www/',
+                        '/home/nico/hackathon/www/ext_libs/',
+                    );
+
+$inc_paths_demo = array(
+                    '/home/apikooien.nl/www/derk/hyves-sync/',
+                    '/home/apikooien.nl/www/derk/hyves-sync/ext_libs/',
+                  );
+
+$path = implode($inc_paths_demo, PATH_SEPARATOR);
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+
 ?>
